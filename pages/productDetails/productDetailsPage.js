ba83783f6af3ec7e8fd93c88a6eea50e86320cd8
anjesagashi <145193704+anjesagashi@users.noreply.github.com>
2025-11-16 11:49:11 +0100

    added main image swap functionality on product details page

--- pages/productDetails/productDetailsPage.js
+++ pages/productDetails/productDetailsPage.js
@@ -24,3 +24,19 @@ minusBtn.addEventListener("click", () => {
   plusBtn.style.backgroundColor = "white";
   plusBtn.style.color = "black";
 });
+
+const mainImage = document.querySelector(".mainImage");
+const fullView = document.querySelector(".fullView img");
+const rightSideAngle = document.querySelector(".rightSideAngle img");
+const extendedView = document.querySelector(".extendedView img");
+const sideView = document.querySelector(".sideView img");
+const changeImage = (thumb) => {
+  const oldSource = mainImage.src;
+  mainImage.src = thumb.src;
+  thumb.src = oldSource;
+};
+
+fullView.addEventListener("click", () => changeImage(fullView));
+rightSideAngle.addEventListener("click", () => changeImage(rightSideAngle));
+extendedView.addEventListener("click", () => changeImage(extendedView));
+sideView.addEventListener("click", () => changeImage(sideView));
